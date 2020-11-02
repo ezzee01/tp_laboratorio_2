34c8ed041d0a36fe67489cb6a86339c188b5474c
ezzee01 <ezec_01@hotmail.com>
2020-11-01 21:26:13 -0300

    TP3

---
+++ TP-03/Carranza.Ezequiel.2C.TP3/Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Test")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("12f8036e-170e-433d-9ac9-f4561d35e374")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TP-03/Carranza.Ezequiel.2C.TP3/Test/UnitTest1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClasesInstanciables;
+using Excepciones;
+using EntidadesAbstractas;
+using System.Collections.Generic;
+
+namespace TestUnitarios
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void DniInvalidoExceptionTest()
+        {
+            //DNI con letra
+            try
+            {
+                Alumno a = new Alumno(01, "ezequiel", "carranza", "37.o18.214", Persona.ENacionalidad.Argentino, Universidad.EClases.Programacion);
+                Assert.Fail();
+            }
+            catch (DniInvalidoException e)
+            {
+                Assert.IsInstanceOfType(e, typeof(DniInvalidoException));
+            }
+
+            //DNI con coma
+            try
+            {
+                Alumno a = new Alumno(01, "ezequiel", "carranza", "37,18214", Persona.ENacionalidad.Argentino, Universidad.EClases.Programacion);
+                Assert.Fail();
+            }
+            catch (DniInvalidoException e)
+            {
+
+                Assert.IsInstanceOfType(e, typeof(DniInvalidoException));
+            }
+
+            //DNI vacio
+            try
+            {
+                Alumno a = new Alumno(01, "ezequiel", "carranza", "", Persona.ENacionalidad.Argentino, Universidad.EClases.Programacion);
+                Assert.Fail();
+            }
+            catch (DniInvalidoException e)
+            {
+
+                Assert.IsInstanceOfType(e, typeof(DniInvalidoException));
+            }
+
+            //DNI fuera de los rangos
+            try
+            {
+                Alumno a = new Alumno(01, "ezequiel", "carranza", "100000000", Persona.ENacionalidad.Argentino, Universidad.EClases.Programacion);
+                Assert.Fail();
+            }
+            catch (DniInvalidoException e)
+            {
+
+                Assert.IsInstanceOfType(e, typeof(DniInvalidoException));
+            }
+
+            try
+            {
+                Alumno a = new Alumno(01, "ezequiel", "carranza", "0", Persona.ENacionalidad.Argentino, Universidad.EClases.Programacion);
+                Assert.Fail();
+            }
+            catch (DniInvalidoException e)
+            {
+
+                Assert.IsInstanceOfType(e, typeof(DniInvalidoException));
+            }
+        }
+
+        [TestMethod]
+        public void SinProfesorExceptionTest()
+        {
+            Universidad uni = new Universidad();
+            try
+            {
+                uni += Universidad.EClases.Laboratorio;
+            }
+            catch (SinProfesorException e)
+            {
+                Assert.IsInstanceOfType(e, typeof(SinProfesorException));
+            }
+        }
+
+        [TestMethod]
+        public void DniInvalido()
+        {
+            Alumno a = new Alumno(0001, "Ezequiel", "Carranza", "37.018.214", Persona.ENacionalidad.Argentino, Universidad.EClases.Programacion);
+            Assert.IsTrue(37018214 == a.DNI);
+
+            Profesor p = new Profesor(0002, "Federico", "Davila", "37014025", Persona.ENacionalidad.Argentino);
+            Assert.IsTrue(37014025 == p.DNI);
+        }
+
+        [TestMethod]
+        public void InstructorNoNulo()
+        {
+            Universidad u = new Universidad();
+            Assert.IsNotNull(u.Instructores);
+        }
+    }
+}
